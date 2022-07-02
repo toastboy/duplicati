8f6040d648e1701d61b5b82b1c2b181ecbfdfc7a
Jon Wilson <github@toastboy.co.uk>
2022-07-02 10:45:34 +0100

    SYS-195 Add teslamate

---
+++ scripts/clean-teslamate-db.sql
@@ -0,0 +1,10 @@
+﻿DROP schema public cascade;
+CREATE schema public;
+CREATE extension cube;
+CREATE extension earthdistance;
+CREATE OR REPLACE FUNCTION public.ll_to_earth(float8, float8)
+    RETURNS public.earth
+    LANGUAGE SQL
+    IMMUTABLE STRICT
+    PARALLEL SAFE
+    AS 'SELECT public.cube(public.cube(public.cube(public.earth()*cos(radians(\$1))*cos(radians(\$2))),public.earth()*cos(radians(\$1))*sin(radians(\$2))),public.earth()*sin(radians(\$1)))::public.earth';
